0d9777d9493e2a3244a0b7bb4729a4eb3e0f16ad
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-06-09 13:21:05 +0200

    PASSATOOOOOOOOO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { BeerCardComponent } from './components/beer-card/beer-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,6 +21,10 @@ import { BeerCardComponent } from './components/beer-card/beer-card.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
